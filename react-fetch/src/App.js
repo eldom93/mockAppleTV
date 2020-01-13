0e0365b4c11f8d3b3110259805cd87bf57de9470
eldom93 <55229671+eldom93@users.noreply.github.com>
2020-01-13 14:52:33 -0800

    add styles to match appletv

--- react-fetch/src/App.js
+++ react-fetch/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">Apple TV</h1>
+        <h1 className="title">Apple TV+</h1>
         <hr />
         <h2 className="heading">My List</h2>
         <div className="row">
@@ -29,8 +29,9 @@ class App extends Component {
             <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
               <div className="card-body">
                 <h5 className="card-title">{todo.title}</h5>
+                <br />
                 <img onMouseOver={this.onMouseOver.bind(this)} className="tv-img" width="100%" height="100%" src={logo}></img>
-                {this.state.hover ? <button className="tv-btn">Remove</button> :  <br />} 
+                {this.state.hover ? <div className="center-btn"> <button className="tv-btn">Remove</button></div> :  <br />} 
               </div>
             </div>
           ))}
