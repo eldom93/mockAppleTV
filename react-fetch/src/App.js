e1648d37595cad0dfb08222df1261c3f78d95010
eldom93 <55229671+eldom93@users.noreply.github.com>
2020-01-13 14:38:12 -0800

    add card styles

--- react-fetch/src/App.js
+++ react-fetch/src/App.js
@@ -23,14 +23,14 @@ class App extends Component {
       <div className="container">
         <h1 className="title">Apple TV</h1>
         <hr />
-        <h2>My List</h2>
+        <h2 className="heading">My List</h2>
         <div className="row">
           {this.state.todos.map((todo) => (
             <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
               <div className="card-body">
                 <h5 className="card-title">{todo.title}</h5>
                 <img onMouseOver={this.onMouseOver.bind(this)} className="tv-img" width="100%" height="100%" src={logo}></img>
-                {this.state.hover ? <button className="tv-remove-btn">Remove</button> :  <br />} 
+                {this.state.hover ? <button className="tv-btn">Remove</button> :  <br />} 
               </div>
             </div>
           ))}
@@ -38,14 +38,14 @@ class App extends Component {
         </div>
         <hr />
 
-          <h2>Recommendations</h2>
+          <h2 className="heading">Recommendations</h2>
           <div className="row">
           {this.state.todos.map((todo) => (
             <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 card">
               <div className="card-body">
                 <h5 className="card-title">{todo.title}</h5>
                 <img onMouseOver={this.onMouseOver.bind(this)} className="tv-img" width="100%" height="100%" src={logo}></img>
-                {this.state.hover ? <button className="tv-remove-btn">Add</button> : <br />} 
+                {this.state.hover ? <button className="tv-btn">Add</button> : <br />} 
               </div>
             </div>
           ))}
